99d1674793d9e9709872db40cefc37e1c67963bc
Maharshi Alpesh <maharshialpesh@gmail.com>
2025-02-19 16:20:27 +0530

    chore: fixing the placement option in toast docs (#4857)

--- apps/docs/content/components/toast/placement.raw.jsx
+++ apps/docs/content/components/toast/placement.raw.jsx
@@ -1,15 +1,26 @@
-import {addToast, Button} from "@heroui/react";
+import {addToast, ToastProvider, Button} from "@heroui/react";
 import React from "react";
 
 export default function App() {
+  const [placement, setPlacement] = React.useState("bottom-right");
+
   return (
-    <div className="flex flex-wrap gap-2">
-      {["top-left", "top-center", "top-right", "bottom-left", "bottom-center", "bottom-right"].map(
-        (position) => (
+    <>
+      <ToastProvider placement={placement} toastOffset={placement.includes("top") ? 60 : 0} />
+      <div className="flex flex-wrap gap-2">
+        {[
+          "top-left",
+          "top-center",
+          "top-right",
+          "bottom-left",
+          "bottom-center",
+          "bottom-right",
+        ].map((position) => (
           <Button
             key={position}
             variant={"flat"}
             onPress={() => {
+              setPlacement(position);
               addToast({
                 title: "Toast title",
                 description: "Toast displayed successfully",
@@ -18,8 +29,8 @@ export default function App() {
           >
             {position}
           </Button>
-        ),
-      )}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
